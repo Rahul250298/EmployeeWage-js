c6ab988e2b52b3f2b045e29fe6fcf1a05ece7a98
rahul <rahulravi2502@gmail.com>
2022-03-16 20:26:02 +0530

    Check employee is present or absent

---
+++ EmployeeWage.js
@@ -0,0 +1,9 @@
+const IS_Absent=0
+let empcheck= Math.floor(Math.random()* 10) % 2;
+if (empcheck == IS_Absent){
+    console.log("Employee is Absent")
+}
+else
+{
+    console.log("Employee is present")
+}

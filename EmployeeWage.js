8fd339db1ed7d1acd41b2b922320a91e6f315dca
rahul <rahulravi2502@gmail.com>
2022-03-17 08:30:19 +0530

    ADD :Array

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -62,8 +62,8 @@ console.log("hour:" +empHrs+ "Emp Wage:" +empwage);
         console.log("UC4 - Total Hrs:" +totalEmpHrs+ "Emp Wage:" +empwage);
 }
 //UC5 For loop
-const NUM_OF_WORKING_DAYS =2;
-let empHrs =0;
+const NUM_OF_WORKING_DAYS = 2;
+let emphrs =0;
 for (let day = 0;
     day< NUM_OF_WORKING_DAYS;day++){
         let empCheck =Math.floor(Math.random() * 10) % 3;
@@ -71,5 +71,26 @@ for (let day = 0;
         }
         let Empwage = empHrs * WAGE_PER_HOUR;
         console.log("Total Hrs:" +empHrs+ "Emp Wage:" +empwage);
+
+ //UC6 Arrays    
+ function calDailyWage(emphrs){
+     return empHrs * WAGE_PER_HOUR;
+ }   
+ const MAX_HRS_IN_MONTH = 160;
+ const NUM_OF_WORKING_DAYS =20;
+ let totalEmpHrs =0;
+ let totalWorkingDays =-0;
+ let empDailyWageArr = new Array();
+
+ while(totalEmpHrs<= MAX_HRS_IN_MONTH && totaWorkingDays<NUM_OF_WORKING_DAYS){
+     totalWorkingDays++;
+     let empcheck = Math.flooor(Math.random()* 10)% 3;
+     let empHrs= getWorkingHours(empCheck);
+     totalEmpHrs += empHrs;
+     empDailyWageArr.push(calDailyWage(totalEmpHrs));
+
+ }
+let empwage = calDailyWage(totalEmpHrs);
+consolr.log("total Days:" + totalWorkingDays + "Total Hrs"+ totalEmpHrs +"Emp Wage"+ empwage;)
 
 
